acb251037be4676560e13c47c3f10b7df6efbbbc
Oleh <oleh59@hotmail.com>
2023-03-07 23:27:54 +0100

    ocultar Key de API

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,11 +1,13 @@
-export const getGifs = async(category, cantidadItems) => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=OdS3QmBe7pjj3IIpnrez0jkPIpcSgmBX&q=${category}&limit=${cantidadItems}`
-    const resp = await fetch(url);
-    const {data} = await resp.json();
-    const gifs = data.map(img => ({
-        id: img.id,
-        title: img.title,
-        url: img.images.downsized_medium.url
-    }))
-return gifs;
-}
+import { KEY } from "./urlKey";
+
+export const getGifs = async (category, cantidadItems) => {
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=${KEY}&q=${category}&limit=${cantidadItems}`;
+  const resp = await fetch(url);
+  const { data } = await resp.json();
+  const gifs = data.map((img) => ({
+    id: img.id,
+    title: img.title,
+    url: img.images.downsized_medium.url,
+  }));
+  return gifs;
+};
